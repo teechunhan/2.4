d5e202fee3563386d84f6af6711f2c57aa50b081
taruc <teech-wm17@student.tarc.edu.my>
2019-11-06 14:21:06 +0800

    a

--- app/src/main/java/MyName.kt
+++ app/src/main/java/MyName.kt
@@ -1,1 +1,3 @@
+package com.example.aboutme
+
 data class MyName(var name: String = "", var nickname: String = "")
--- app/src/main/java/com/android/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/android/example/aboutme/MainActivity.kt
@@ -1,25 +1,15 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.example.aboutme
 
+import android.content.Context
+import android.databinding.DataBindingUtil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.android.aboutme.databinding.ActivityMainBinding
 import androidx.databinding.DataBindingUtil
+import com.android.example.aboutme.databinding.ActivityMainBinding
+
 /**
  * Main Activity of the AboutMe app. This app demonstrates:
  *     * LinearLayout with TextViews, ImageView, and ScrollView
@@ -27,27 +17,36 @@ import androidx.databinding.DataBindingUtil
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val myName: MyName = MyName("Aleks Haecky")
-
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.doneButton.setOnClickListener {
             addNickname(it)
-            binding.nicknameText.text = binding.nicknameEdit.text
-            binding.nicknameEdit.visibility = View.GONE
-            binding.doneButton.visibility = View.GONE
-            binding.nicknameText.visibility = View.VISIBLE
-            binding.myName?.nickname = nicknameEdit.text.toString()
-            binding.myName = myName
         }
+    }
+
+    private fun addNickName(view:view){
         binding.apply {
             myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
-            nicknameText.text = nicknameEdit.text.toString()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
         }
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, flags: 0)
+    }
+
+    private fun updateNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+        editText.requestFocus()
+        val imn = getSystemService(Context.INPUT_METHOD_SERVICE) as  inputMethodManager
+        imn.showSoftInput(editText,flags: 0)
     }
 }
